de41f4b7c1b80106a9133548e33d9ce8590e3d9c
joaoand <joao.demata@correo.unimet.edu.ve>
2023-08-29 22:44:21 -0400

    navbar y link de spa

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "../components/navbar/navbar";
 
 const About = () => {
     return (
         <div>
-            <h1>Esta es la pagina de about</h1>
+            <Navbar/>
+            <p>Esta es la pagina de about</p>
         </div>
     );
 };
